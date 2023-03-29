8a19b0c5870718eb9665d28fb595dc4f8aeb0b00
Varnik103 <varnik103@gmail.com>
2023-03-29 23:32:10 +0530

    Updated client

---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="ProfileSide">Profile</div>
+      <div className="PostSide">Post</div>
+      <div className="RigthSide">Right</div>
+    </div>
+  )
+}
+
+export default Home
