2c6e852f11cc07a92c24094688b40f8f0aaa7301
Ansh1435 <anshgoel19093@gmail.com>
2023-03-30 17:21:47 +0530

    implemented models

---
+++ server/models/postSchema.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+
+const postSchema = mongoose.model("postSchema", new mongoose.Schema({
+	userid: { type: String, allowNull: false, required: [true, 'userid is required'] },
+    description: {type: String, allowNull: false, required: [true, 'description  is required ']},
+    likes:[],
+    createdAt:{type:Date},
+    image: {type: String, allowNull: true },
+    
+}, { timestamps: true }));
+const doc = new postSchema();
+module.exports = postSchema;
+
+
+
