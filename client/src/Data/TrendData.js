0358e310094fee2a828effcd9c977c2d243d2e2c
Varnik103 <varnik103@gmail.com>
2023-03-30 22:49:04 +0530

    Implemented RightSide

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData=[
+    {
+        name: "Minions",
+        shares: 97767,
+    },
+    {
+        name: "Avengers",
+        shares: 97807,
+    },
+    {
+        name: "Memes",
+        shares: 6546697,
+    },
+    {
+        name: "Selmon Bhai",
+        shares: 99999999999,
+    },
+    {
+        name: "Cllge online krdo",
+        shares: 977,
+    }
+]
--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import home from '../../img/home.png'
+import {AiOutlineSetting} from 'react-icons/ai'
+import noti from '../../img/noti.png'
+import comment from '../../img/comment.png'
 import './RightSide.css'
+import TrendCard from '../TrendCard/TrendCard'
 
 const RightSide = () => {
   return (
     <div className="RightSide">
         <div className="navIcons">
-            
+            <img src={home} alt="" />
+            <AiOutlineSetting  className='setting'/>
+            <img src={noti} alt="" />
+            <img src={comment} alt="" />
         </div>
+        <TrendCard/>
+        <button className='button r-button'> 
+            Share
+        </button>
     </div>
   )
 }
